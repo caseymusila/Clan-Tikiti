43be05412b2b9405f2eb2bb58debd96105b8312e
caseymusila <musilacasey@gmail.com>
2021-10-01 07:39:31 +0300

    in our models we create our imports model

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -4,7 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.loginPage, name='login'),
-    path('register/',views.registerPage, name='register')
+    path('register/',views.registerPage, name='register'),
     path('home/',views.home, name='home'),
     path('create_event/', views.create_event, name='create_event'),
     path('disp_event/<project_id>',views.disp_event, name='disp_event')
