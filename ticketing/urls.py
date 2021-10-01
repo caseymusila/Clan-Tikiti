807dadbc50bdb978c6ae13feaa089e49b8b6fe0f
caseymusila <musilacasey@gmail.com>
2021-10-01 07:38:06 +0300

    we register our login to urls to be the default

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.loginPage, name='login'),
+    path('', views.loginPage, name='login')
     path('register/',views.registerPage, name='register'),
     path('home/',views.home, name='home'),
     path('create_event/', views.create_event, name='create_event'),
