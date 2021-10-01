c6cc2ef2a49636d2b2689d0e29853f823480a74e
caseymusila <musilacasey@gmail.com>
2021-10-01 07:38:43 +0300

    we register also our signup page to urls

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.loginPage, name='login')
-    path('register/',views.registerPage, name='register'),
+    path('', views.loginPage, name='login'),
+    path('register/',views.registerPage, name='register')
     path('home/',views.home, name='home'),
     path('create_event/', views.create_event, name='create_event'),
     path('disp_event/<project_id>',views.disp_event, name='disp_event')
